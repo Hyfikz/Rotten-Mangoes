b7b04d53c9da905cae97c369f4fd91146085a126
Brett Weinkauf <bweinkau@ualberta.ca>
2016-04-26 00:02:20 +0000

    Basic CRUD functionality in palce

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RottenMangoes::Application.routes.draw do
+
+  resources :movies
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -39,7 +41,7 @@ RottenMangoes::Application.routes.draw do
   #       get 'recent', on: :collection
   #     end
   #   end
-  
+
   # Example resource route with concerns:
   #   concern :toggleable do
   #     post 'toggle'
